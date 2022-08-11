39018ad137d7371c87293c94c1f8ac819ebe6c3d
insovaidev <insovaidev@gmail.com>
2022-08-11 14:25:08 +0700

    test

---
+++ precache-manifest.6c8509b1d349bcd0c6895c09421ad2b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daeabf3b3b9463f616b5d1cb445b84a9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ad763b3aaaf3493eecb",
+    "url": "./static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "e75a4f09ca0d5f1a54ae",
+    "url": "./static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "9ad763b3aaaf3493eecb",
+    "url": "./static/js/2.089b8955.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "./static/js/2.089b8955.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e75a4f09ca0d5f1a54ae",
+    "url": "./static/js/main.f57753b3.chunk.js"
+  },
+  {
+    "revision": "d25113d24f590e04e053",
+    "url": "./static/js/runtime-main.80763978.js"
+  },
+  {
+    "revision": "c3231f1d2db52ef43453a0b75918d3e7",
+    "url": "./static/media/profile.c3231f1d.jpg"
+  }
+]);
